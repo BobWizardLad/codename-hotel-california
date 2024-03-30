d63b5094bdebbcd17b6d7b0986bd5fc4e53aeabb
Bobwizardlad <bobwizardlad@gmail.com>
2024-03-30 07:22:18 -0500

    :construction: WIP UI/Combat
    Cleaning up the UI.

--- assets/scripts/player_ui.gd
+++ assets/scripts/player_ui.gd
@@ -1,19 +1,24 @@
 extends Control
 
-@onready var popup_dialog: Label = $PopupDialog
+# External Nodes
+@onready var PLAYER: Node = get_node("../Player")
+
+# Child Nodes
+@onready var POPUP_DIALOG: Label = $PopupDialog
+#@onready var 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	popup_dialog.hide()
+	POPUP_DIALOG.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _on_player_popup_interact(msg: String):
-	popup_dialog.text = msg
-	popup_dialog.show()
+	POPUP_DIALOG.text = msg
+	POPUP_DIALOG.show()
 
 func _on_player_popup_close():
-	popup_dialog.text = ""
-	popup_dialog.hide()
+	POPUP_DIALOG.text = ""
+	POPUP_DIALOG.hide()
